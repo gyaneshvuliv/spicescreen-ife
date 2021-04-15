93f7a2d61db9cad35b743d773bf00d70976661d4
unknown <kedargdr@gmail.com>
2021-04-15 11:55:54 +0530

    change port no

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -19,7 +19,7 @@ const all = {
   root: path.normalize(__dirname + '/../../..'),
 
   // Server port
-  port: process.env.PORT || 9632,
+  port: process.env.PORT || 9091,
 
   // Server IP
   ip: process.env.IP || '0.0.0.0',
